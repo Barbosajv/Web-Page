95dc35e33ba42f9e65cfc4942cdef2a3bba24118
João Vitor Santos Barbosa <J.vitorsb@hotmail.com>
2022-06-21 18:22:33 -0300

    Add files via upload
    login page fixed

--- home screen/assets/login.js
+++ home screen/assets/login.js
@@ -1,19 +1,29 @@
-const darkModeClass = 'dark-mode'
-const lua = document.querySelector('#lua')
-const sol = document.querySelector('#sol')
-const body = document.querySelector('body'[0])
+const darkModeClass = 'dark-mode';
+const lua = document.querySelector('#lua');
+const sol = document.querySelector('#sol');
+const h1 = document.querySelector('#fh1');
+const p = document.querySelector('#p1');
+const a = document.querySelector('#a');
+const btn = document.querySelector('#btn');
+const principal = document.querySelector('.principal');
 
 
 function changeMode(){
-    changeClasses()
+    changeClasses();
 }
 
 
 function changeClasses(){
-    lua.classList.toggle(darkModeClass)
+    lua.classList.toggle(darkModeClass);
+    sol.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    p.classList.toggle(darkModeClass);
+    a.classList.toggle(darkModeClass);
+    btn.classList.toggle(darkModeClass);
+    principal.classList.toggle(darkModeClass);
 }
 
-lua.addEventListener('click', changeMode)
+lua.addEventListener('click', changeMode);
 sol.addEventListener("click", changeClasses);
 
 
